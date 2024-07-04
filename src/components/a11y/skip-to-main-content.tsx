a3cdf47b99eb9ca707514c61ea7b1e1c055a7ee5
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-04 11:53:55 -0400

    add skipt to main content component and story

---
+++ src/components/a11y/skip-to-main-content.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export interface SkipToMainContentProps {
+  id: string;
+  className: string;
+}
+
+export const SkipToMainContent = ({ id, className }: SkipToMainContentProps) =>
+  <a href={`#${id}`} className={`absolute top-0 -left-[99999px] focus-visible:left-5 ${className ? className : ''}`}>Skip to main content</a>;
+
---
+++ src/stories/SkipToMainContent.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { SkipToMainContent, SkipToMainContentProps } from '../components/a11y/skip-to-main-content';
+
+export default {
+  title: 'components/a11y/skip-to-main-content',
+  component: SkipToMainContent,
+} as Meta;
+
+const Template: StoryFn<SkipToMainContentProps> = (args) => <SkipToMainContent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 'main-content',
+  className: 'text-red'
+};
