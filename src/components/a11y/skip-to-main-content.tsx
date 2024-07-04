246e43d51f14eaa89ff65ca651e25e20b2d817f0
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-04 11:22:24 -0400

    remove files, start fresh

--- src/components/a11y/skip-to-main-content.tsx
+++
@@ -1,4 +0,0 @@
-const SkipToMainContent = ({ id, className }: { id: string; className?: string; }) =>
-  <a href={`#${id}`} className={`absolute top-0 -left-[99999px] focus-visible:left-5 ${className ? className : ''}`}>Skip to main content</a>;
-
-export default SkipToMainContent;
