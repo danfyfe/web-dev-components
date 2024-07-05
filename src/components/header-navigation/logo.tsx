0d10e60c5217401f596a1437b0d676227fb1551d
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-05 10:58:43 -0400

    add logo styling for layout

--- src/components/header-navigation/logo.tsx
+++ src/components/header-navigation/logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LogoProps } from ".";
 
 const Logo = ({ logo }: { logo: LogoProps }) => (
-  <div>
+  <div className="flex justify-center items-center">
     <a href="/" aria-label={logo.label ? logo.label : logo.alt}>
       <img alt={logo.alt} src={logo.src} height={logo.height ?? undefined} width={logo.width ?? undefined} />
     </a>
