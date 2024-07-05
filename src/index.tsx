6a2dc9a48d96f5a6ab091c768e105588663b1da7
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-05 10:35:46 -0400

    add export for headernav, boost version

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { SkipToMainContent } from './components/a11y/skip-to-main-content';
+import { HeaderNavigation } from './components/header-navigation';
 
 export {
-  SkipToMainContent
+  SkipToMainContent,
+  HeaderNavigation
 }
