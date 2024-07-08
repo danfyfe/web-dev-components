c5c4544bcd5421235a0a12e6ef04b54a4d585e39
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-08 16:02:06 -0400

    update tailwind config for future module use

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,34 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#89CFF0',
+        secondary: '#808080',
+        tertiary: '#6082B6',
+        success: '#00A36C',
+        error: '#FF5733',
+        text: '#444'
+      },
+      fontFamily: {
+        heading: '',
+        subheading: '',
+        paragraph: 'Arial, Helvetica, sans-serif;'
+      },
+      fontSize: {
+        h1: '2.5rem',
+        h2: '2rem',
+        h3: '1.75rem',
+        h4: '1.5rem',
+        h5: '1.25rem',
+        h6: '1rem',
+        link: '1rem',
+        paragraph: '1rem',
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
-
