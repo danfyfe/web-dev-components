0f408c985ab06480c522b460f74e7c4b32a16091
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-08 16:03:16 -0400

    update exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,25 @@ import { SkipToMainContent } from './components/a11y/skip-to-main-content';
 import { HeaderNavigation } from './components/header-navigation';
 import { MainContainer } from './components/containers/main';
 import { SectionContainer } from './components/containers/section';
+import { H1 } from './components/typography/H1';
+import { H2 } from './components/typography/H2';
+import { H3 } from './components/typography/H3';
+import { H4 } from './components/typography/H4';
+import { H5 } from './components/typography/H5';
+import { H6 } from './components/typography/H6';
+import { P } from './components/typography/P';
+
 
 export {
   SkipToMainContent,
   HeaderNavigation,
   MainContainer,
-  SectionContainer
+  SectionContainer,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  P
 };
